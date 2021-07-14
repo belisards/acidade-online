9e504f16f6c7e66ba28c459f1e04ca9d76f71c89
belisards <adrianobf@gmail.com>
2021-07-13 21:20:21 -0300

    fix scraper

--- acidadeonline.py
+++ acidadeonline.py
@@ -6,10 +6,16 @@ def scrape_news(url):
     response = requests.get(url)
     doc = BeautifulSoup(response.text, 'html.parser')
     article = []
-    for paragraph in doc.find_all(class_='post__description')[0].findAll('p'):
-        article.append(paragraph.text)
-    news = '\n'.join(article)
-    return news
+    # print('Content downloaded',url)
+    all_paragraphs = doc.find_all(class_='post__description')
+    if len(all_paragraphs) > 0:
+        for paragraph in all_paragraphs[0].findAll('p'):
+            try:
+                article.append(paragraph.text)
+            except:
+                print("No text found") 
+        news = '\n'.join(article)
+        return news
 
 response = requests.get('https://www.jornaldacidadeonline.com.br/')
 
@@ -27,6 +33,6 @@ for item in doc.find_all(class_='widget__data'):
 
 df = pd.DataFrame(rows)
 
-# df['text'] = df.link.apply(scrape_news)
+df['text'] = df.link.apply(scrape_news)
 
-pd.read_csv('jornaldacidade.csv').append(df).drop_duplicates().to_csv('jornaldacidade.csv')
+pd.read_csv('jornaldacidade.csv').append(df).drop_duplicates().to_csv('jornaldacidade.csv', index=False)
